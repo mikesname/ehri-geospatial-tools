b0248746f42161ff6695400e51fb838ea1716457
Mike Bryant <mikesname@gmail.com>
2022-07-01 17:13:59 +0100

    Handle some more edge cases (multiple layers, tile layers etc)
    Relying on Geoserver to create layers rather than using the API
    ourselves.

--- gpkg2gs.py
+++ gpkg2gs.py
@@ -2,9 +2,9 @@ import json
 import os
 import sqlite3
 import urllib.parse
-from collections import namedtuple
 from http import HTTPStatus
-from typing import List
+from typing import List, Tuple
+from typing import NamedTuple
 
 import geopandas.io.file
 import requests
@@ -12,7 +12,17 @@ import slugify
 import streamlit as st
 from geopandas import GeoDataFrame
 
-LayerInfo = namedtuple("LayerInfo", ["table_name", "data_type", "identifier", "description"])
+
+class LayerInfo(NamedTuple):
+    table_name: str
+    data_type: str
+    identifier: str
+    description: str
+    bounds: Tuple[float, float, float, float]
+    srs: str
+
+    def __str__(self):
+        return self.identifier
 
 
 def get_layer_info(filepath: str) -> List[LayerInfo]:
@@ -20,55 +30,28 @@ def get_layer_info(filepath: str) -> List[LayerInfo]:
     layers = []
     try:
         cursor = conn.cursor()
-        for tn, dt, ident, desc in cursor.execute(
-                "SELECT table_name, data_type, identifier, description FROM gpkg_contents"):
-            layers.append(LayerInfo(tn, dt, ident, desc))
-    except sqlite3.DatabaseError:
-        st.error(f"Error reading GeoPackage '{filepath}' (is it the right format?)")
+        for tn, dt, ident, desc, min_x, min_y, max_x, max_y, srs_id, auth in cursor.execute(
+                """SELECT
+                    c.table_name,
+                    c.data_type,
+                    c.identifier,
+                    c.description,
+                    c.min_x,
+                    c.min_y,
+                    c.max_x,
+                    c.max_y,
+                    c.srs_id,
+                    s.organization
+                    FROM gpkg_contents c
+                    JOIN gpkg_spatial_ref_sys s
+                    ON c.srs_id = s.srs_id"""):
+            layers.append(LayerInfo(tn, dt, ident, desc, (min_x, min_y, max_x, max_y), f"{auth}:{srs_id}"))
+    except sqlite3.DatabaseError as e:
+        st.error(f"Error reading GeoPackage '{filepath}' (is it the right format?) {e}")
         st.stop()
     finally:
         conn.close()
     return layers
-
-
-def create_or_update_layer(session: requests.Session, base_url: str, workspace: str, store: str,
-                           layer: LayerInfo) -> bool:
-    """Create a new layer from the GPKG-derived layer data"""
-    st.info(f"Importing layer '{layer.identifier}'")
-    list_url = f"{base_url}/datastores/{store}/featuretypes"
-    r = session.get(list_url)
-    layers = []
-    try:
-        layers = [data["name"] for data in r.json().get("featureTypes", {}).get("featureType", [])]
-    except AttributeError:
-        # no layers
-        pass
-
-    url = list_url
-    method = "POST"
-    if layer.identifier in layers:
-        url = f"{base_url}/datastores/{store}/featuretypes/{layer.identifier}"
-        method = "PUT"
-
-    data = dict(
-        featureType=dict(
-            name=layer.identifier,
-            nativeName=layer.table_name,
-            namespace=dict(
-                name=workspace,
-                href=f"{base_url}.json"
-            ),
-            title=layer.identifier,
-            description=layer.description,
-            keywords=dict(string=[layer.data_type])
-        )
-    )
-
-    r = session.request(method, url, data=json.dumps(data))
-    if r.status_code not in [HTTPStatus.OK, HTTPStatus.CREATED]:
-        st.error(f"Unexpected import status code: {r.status_code} ({method} {url})")
-        st.stop()
-    return layer.identifier in layers
 
 
 def fix_store_data(session: requests.Session, base_url: str, name: str, workspace: str):
@@ -114,53 +97,87 @@ def fix_store_data(session: requests.Session, base_url: str, name: str, workspac
         st.stop()
 
 
-def ingest_store(session: requests.Session, base_url: str, name: str, uploaded_file: str):
-    """Initial data ingest: this requires fixing later..."""
-    st.info("Importing GeoPackage...")
-    ingest_url = f"{base_url}/datastores/{name}/file.gpkg"
-    ingest_resp = session.put(ingest_url, data=uploaded_file, headers={"content-type": "application/x-sqlite3"})
-    if ingest_resp.status_code not in [HTTPStatus.OK, HTTPStatus.CREATED, HTTPStatus.ACCEPTED]:
-        st.error(f"Unexpected status code from ingest: {ingest_resp.status_code}")
-        st.write(ingest_resp.text)
+def ingest_store(session: requests.Session, base_url: str, workspace: str, name: str, data_type: str,
+                 uploaded_file: str):
+    """Coverage data ingest..."""
+    ext = "gpkg"
+    endpoint = "datastores"
+    store_name = name
+    if data_type == "tiles":
+        # NB: weird "extension" required here...
+        ext = "geopackage (mosaic)"
+        endpoint = "coveragestores"
+        store_name = f"{name}_{data_type}"
+    st.info(f"Importing {ext}...")
+    url = f"{base_url}/{endpoint}/{store_name}/file.{ext}"
+    resp = session.put(url, data=uploaded_file, headers={"content-type": "application/x-sqlite3"})
+    if resp.status_code not in [HTTPStatus.OK, HTTPStatus.CREATED, HTTPStatus.ACCEPTED]:
+        st.error(f"Unexpected status code from ingest: {resp.status_code}")
+        st.write(resp.text)
         st.stop()
 
+    # Correct datastore metadata...
+    if endpoint == "datastores":
+        fix_store_data(session, base_url, store_name, workspace)
 
-def get_wms_url(gdf, proto, host, workspace, name):
-    bounds = gdf.total_bounds
+
+def preview_layer(filename: str, layer: LayerInfo, uploaded_file):
+    if not layer.data_type == "features":
+        st.warning(f"No preview available for data type: {layer.data_type}")
+        return
+
+    with st.spinner(f"Loading data for layer \"{layer.identifier}\"..."):
+        gdf = load_dataframe(filename, uploaded_file.id, layer=layer.table_name)
+        is_points = check_point_geom(gdf)
+        if is_points:
+            show = st.radio(f"Preview data for layer \"{layer.identifier}\":", ("Table", "Map"))
+            if show == "Table":
+                st.write(gdf)
+            else:
+                st.map(clone_with_lat_lon(gdf))
+        else:
+            st.write(gdf)
+            st.warning("Map preview only available with Point geometry")
+
+        st.write("---")
+
+
+def get_wms_url(proto: str, host: str, workspace: str, layer_info: LayerInfo):
+    bounds = layer_info.bounds
     ratio = float(bounds[3] - bounds[1]) / float(bounds[2] - bounds[0])
     width = 1024
     height = int(float(width) * ratio)
     bbox = ','.join([str(p) for p in bounds])
     params = dict(
         request="GetMap",
-        layers=f"{workspace}:{name}",
+        layers=f"{workspace}:{layer_info.table_name}",
         bbox=bbox,
         version="1.1.1",
         service="wms",
         width=width,
         height=height,
         format="image/jpeg",
-        srs=gdf.crs.srs
+        srs=layer_info.srs
     )
     return f"{proto}://{host}/geoserver/wms?{urllib.parse.urlencode(params)}"
 
 
 @st.cache(show_spinner=False)
-def load_dataframe(path: str, *_):
-    return geopandas.read_file(path)
+def load_dataframe(path: str, *_, **kwargs):
+    return geopandas.read_file(path, **kwargs)
 
 
 @st.cache(show_spinner=False)
 def clone_with_lat_lon(gdf: GeoDataFrame) -> GeoDataFrame:
     df = gdf.copy(deep=True)
-    df['lon'] = df.geometry.x
-    df['lat'] = df.geometry.y
+    df["lon"] = df.geometry.x
+    df["lat"] = df.geometry.y
     return df
 
 
 def check_point_geom(gdf) -> bool:
     import numpy as np
-    return np.array_equal(np.array(['Point']), gdf.geom_type.unique())
+    return np.array_equal(np.array(["Point"]), gdf.geom_type.unique())
 
 
 def main():
@@ -193,22 +210,16 @@ def main():
         st.error("No layers found, exiting...")
         st.stop()
 
-    st.info(f"Layers found: {len(layers)}")
+    st.info(f"Layers found: {', '.join([layer.identifier for layer in layers])}")
+    data_types = set([layer.data_type for layer in layers])
 
-    with st.spinner("Loading data..."):
-        gdf = load_dataframe(filename, uploaded_file.id)
-        is_points = check_point_geom(gdf)
-        if is_points:
-            show = st.radio("Preview data:", ('Table', 'Map'))
-            if show == 'Table':
-                st.write(gdf)
-            else:
-                st.map(clone_with_lat_lon(gdf))
-        else:
-            st.write(gdf)
-            st.warning("Map preview only available with Point geometry")
+    if "features" not in data_types:
+        st.warning("No feature layers found, preview will not be available")
 
-        st.write("---")
+    layer = layers[0]
+    if len(layers) > 1:
+        layer = st.selectbox("Preview layer:", layers)
+    preview_layer(filename, layer, uploaded_file)
 
     if st.button(f"Import GeoPackage '{filename}'"):
         session = requests.Session()
@@ -221,33 +232,22 @@ def main():
         base_url = f"{proto}://{host}/geoserver/rest/workspaces/{workspace}"
 
         st.info(f"Workspace: `{workspace}`; Base URL: `{base_url}`")
-
-        progress = st.progress(0)
-        num_ops = 0
-        inc_progress = 100 / (2 + len(layers))
-
-        ingest_store(session, base_url, name, uploaded_file)
-        num_ops += 1
-        progress.progress(int(inc_progress * num_ops))
-
-        fix_store_data(session, base_url, name, workspace)
-        num_ops += 1
-        progress.progress(int(inc_progress * num_ops))
-
-        for layer_info in layers:
-            create_or_update_layer(session, base_url, workspace, name, layer_info)
-            num_ops += 1
-            progress.progress(int(inc_progress * num_ops))
+        for data_type in data_types:
+            st.info(f"Uploading store for data type: {data_type}")
+            ingest_store(session, base_url, workspace, name, data_type, uploaded_file)
+            uploaded_file.seek(0)
 
         st.balloons()
         st.success("Done!")
 
-        with st.spinner("Loading layer preview..."):
-            for layer_info in layers:
-                wms_url = get_wms_url(gdf, proto, host, workspace, layer_info.identifier)
-                with st.container():
-                    st.markdown(f"WMS layer preview: '{layer_info.identifier}' ([link]({wms_url}))")
-                    st.image(wms_url)
+        for layer_info in layers:
+            if layer_info.data_type in ["features", "tiles"]:
+                with st.spinner("Loading GeoServer layer preview..."):
+                    wms_url = get_wms_url(proto, host, workspace, layer_info)
+                    st.write("---")
+                    with st.container():
+                        st.markdown(f"WMS layer preview: '{layer_info.identifier}' ([link]({wms_url}))")
+                        st.image(wms_url)
 
 
 if __name__ == "__main__":
