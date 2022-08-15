08f93413e1927cdf3e7c31a432f0dc27af5d4d29
Mike Bryant <mikesname@gmail.com>
2022-08-15 12:08:20 +0100

    Fix argument specifications

--- batch.py
+++ batch.py
@@ -10,17 +10,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Import GeoPackage files into GeoServer.')
     parser.add_argument('files', type=str, nargs='+',
                         help='one or more .gpkg files to import')
-    parser.add_argument('--host', dest='host', type=str, action='store',
+    parser.add_argument('-H', '--host', dest='host', type=str, action='store',
                         default="localhost:8080", help='the GeoServer host')
-    parser.add_argument('-u|--username', dest='username', type=str, action='store',
+    parser.add_argument('-u', '--username', dest='username', type=str, action='store',
                         default="admin", help='the GeoServer user')
-    parser.add_argument('-p|--password', dest='password', type=str, action='store',
+    parser.add_argument('-p', '--password', dest='password', type=str, action='store',
                         default="geoserver", help='the GeoServer password')
-    parser.add_argument('-w|--workspace', dest='workspace', type=str, action='store',
+    parser.add_argument('-w', '--workspace', dest='workspace', type=str, action='store',
                         help='the GeoServer host')
-    parser.add_argument('-s|--secure', dest='secure', action='store_true',
+    parser.add_argument('-s', '--secure', dest='secure', action='store_true',
                         default=False, help='whether GeoServer is using HTTPS')
-    parser.add_argument('-v|--verbose', dest='verbose', action='store_true',
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
                         default=False, help='show verbose output')
 
     args = parser.parse_args()
